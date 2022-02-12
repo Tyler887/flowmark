0c026bdfae8ec194a494d90a07a7a7d6f525a9b5
Tyler887 <tylermageeshields@gmail.com>
2022-02-12 16:25:00 +0000

    Create gweb.py

---
+++ gweb.py
@@ -0,0 +1,13 @@
+print("GWeb v1.0")
+import os
+print("Setting up...")
+os.system("pip install gh-md-to-html colorama")
+print(Fore.YELLOW + "Note: GWeb only generates files in GFM format." + Style.RESET_ALL)
+cd = os.path.dirname(os.path.realpath(__file__))
+import glob
+files = glob.glob(cd + "/*.md")
+import gh_md_to_html
+for i in files.nditer(b):
+    if not os.path.exists('/tmp/test'):
+      with open(i.replace('.md', '.html', 'w'): pass
+    print("Generated " + i ".")
