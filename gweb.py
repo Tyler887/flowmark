918de8aa4e999ecd81d67c0dc099acc1f66adaa6
Tyler887 <tylermageeshields@gmail.com>
2022-02-12 16:28:56 +0000

    Update gweb.py

--- gweb.py
+++ gweb.py
@@ -8,6 +8,5 @@ import glob
 files = glob.glob(cd + "/*.md")
 import gh_md_to_html
 for i in files.nditer(b):
-    if not os.path.exists('/tmp/test'):
-      open(i.replace('.md', '.html')
+    open(i.replace('.md', '.html')
     print("Generated " + i + ".")
